2faf6e931a069ab2ec0c307a56e1c5a6d3470326
Ian Skelskey <ianskelskey@gmail.com>
2022-08-14 01:27:39 -0400

    Remove unused tests

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:fitness_tracker/main.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-  });
-}
